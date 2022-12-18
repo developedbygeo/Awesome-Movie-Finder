cae88ea78bd24f31bfb0d09752e960f9d8f8775c
georgiosan <georgiosanagnostou@hotmail.com>
2022-12-19 00:55:09 +0100

    chore: updating configs

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,12 +2,12 @@ const micromatch = require('micromatch');
 
 module.exports = {
   // Lint & Prettify JS & JSX files
-  '**/*.(js|jsx)': (filenames) => [
-    `eslint --fix ${filenames.join(' ')}`,
-    `npx prettier --write ${filenames.join(' ')}`,
-  ],
+  '**/*.(js|jsx)': (filenames) => {
+    const match = micromatch.not(filenames, ['*.eslintrc.js', '*.lintstagedrc.js', '*.{config.js,config.ts}']);
+    return [`eslint --fix ${match.join(' ')}`, `npx prettier --write ${match.join(' ')}`];
+  },
 
-  // Prettify only Markdown and JSON files
+  // Prettify only Markdown and JSON files apart from package.json and package-lock.json
   '**/*.(md|json)': (filenames) => {
     const match = micromatch.not(filenames, ['*package.json', '*package-lock.json']);
     return `npx prettier --write ${match.join(' ')}`;
