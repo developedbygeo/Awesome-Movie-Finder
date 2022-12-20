ce2b44483a8986ff4f8d97f5801bb7726f7900e1
georgiosan <georgiosanagnostou@hotmail.com>
2022-12-20 01:02:49 +0100

    chore: adding base animations

---
+++ utils/animations.js
@@ -0,0 +1,42 @@
+export const navWrapper = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.7,
+      duration: 0.5,
+      when: 'beforeChildren',
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+export const navItem = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.25,
+    },
+  },
+};
+
+export const scaleVariant = {
+  hidden: {
+    scale: 0,
+    opacity: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
