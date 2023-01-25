2c103de277260693f1c6b97569a75b376335ad9f
georgiosan <georgiosanagnostou@hotmail.com>
2023-01-25 19:39:58 +0200

    chore: cleanup

--- components/UI/NavMenu.jsx
+++
@@ -1,9 +0,0 @@
-import { AnimatePresence } from 'framer-motion';
-
-import MobileNav from '@/components/UI/MobileNav';
-
-const NavMenu = ({ isOpen }) => (
-  <AnimatePresence>{isOpen && <MobileNav key="mobileNav" exit={{ opacity: 0 }} />};</AnimatePresence>
-);
-
-export default NavMenu;
