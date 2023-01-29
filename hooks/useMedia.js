deea671ff13eaede82fb74a54506a8ca71d49d9a
georgiosan <georgiosanagnostou@hotmail.com>
2023-01-30 00:53:36 +0200

    feat: adding the usemedia hook

---
+++ hooks/useMedia.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+function useMedia(query) {
+  const [matches, setMatches] = useState(
+    typeof window !== 'undefined' ? window.matchMedia(query).matches : false
+  );
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    media.addEventListener('change', listener);
+    return () => media.removeEventListener('change', listener);
+  }, [query, matches]);
+
+  return matches;
+}
+
+export default useMedia;
