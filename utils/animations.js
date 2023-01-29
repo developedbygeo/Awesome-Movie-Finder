56f259a876dfdd6acbeadfca13211d5ed755beca
georgiosan <georgiosanagnostou@hotmail.com>
2023-01-30 01:01:32 +0200

    fix: updating burgervariants

--- utils/animations.js
+++ utils/animations.js
@@ -1,14 +1,49 @@
-export const navWrapper = {
+export const fadeInWithChildren = {
   hidden: {
     opacity: 0,
   },
   visible: {
     opacity: 1,
     transition: {
-      delay: 0.7,
+      delay: 0.5,
       duration: 0.5,
       when: 'beforeChildren',
-      staggerChildren: 0.5,
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+export const pageFadeIn = {
+  ...fadeInWithChildren,
+  visible: {
+    ...fadeInWithChildren.visible,
+    transition: {
+      ...fadeInWithChildren.visible.transition,
+      staggerChildren: 0.45,
+    },
+  },
+};
+
+export const navWrapperMobile = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      ease: 'easeInOut',
+      when: 'beforeChildren',
+      staggerChildren: 0.15,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+      ease: 'easeInOut',
+      when: 'afterChildren',
+      staggerChildren: 0.15,
     },
   },
 };
@@ -22,21 +57,106 @@ export const navItem = {
     opacity: 1,
     y: 0,
     transition: {
+      duration: 0.15,
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: 20,
+    transition: {
       duration: 0.25,
+    },
+  },
+};
+
+export const itemFadeIn = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.25,
+    },
+  },
+};
+
+export const simpleFadeIn = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.25,
+      delay: 2.25,
     },
   },
 };
 
 export const scaleVariant = {
   hidden: {
-    scale: 0,
     opacity: 0,
   },
   visible: {
-    scale: 1,
     opacity: 1,
     transition: {
+      ease: 'linear',
       duration: 0.5,
+    },
+  },
+};
+
+export const burgerVariants = {
+  container: {
+    closed: {
+      rotate: 0,
+    },
+    open: {
+      rotate: 2,
+    },
+  },
+  top: {
+    closed: {
+      rotate: 0,
+      y: 0,
+    },
+    open: {
+      rotate: 45,
+      x: '0.25rem',
+      y: 10,
+    },
+  },
+  middle: {
+    closed: {
+      opacity: 1,
+    },
+    open: {
+      opacity: 0,
+    },
+  },
+  bottom: {
+    mobile: {
+      closed: {
+        rotate: 0,
+        y: 0,
+      },
+      open: {
+        rotate: -45,
+        y: '-0.4rem',
+        x: '0.25rem',
+      },
+    },
+    tablet: {
+      closed: {
+        rotate: 0,
+        y: 0,
+      },
+      open: {
+        rotate: -45,
+        y: '-0.8rem',
+        x: '0.25rem',
+      },
     },
   },
 };
